97cdfcd081eecf88ae70d7acf5a7a66ec668e8aa
geomwangi007 <geoffmwangi125@gmail.com>
2024-11-13 07:28:00 +0300

    adjusting the files

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -1,44 +1,26 @@
 import streamlit as st
 import pandas as pd
-from model.model_training import train_model
-from utils.data_handling import load_data
 import pickle
+from modules.data_preprocessing import load_and_merge_data, apply_mappings, transform_data, preprocess_data
+from modules.training import create_pipeline, evaluate_model
 
-st.markdown(
-    """
-    <style>
-        .reportview-container .main .block-container{
-            max-width: 90%;
-            padding-top: 5rem;
-            padding-right: 5rem;
-            padding-left: 5rem;
-            padding-bottom: 5rem;
-        }
-        img{
-            max-width:40%;
-            margin-bottom:40px;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+# Load trained model
+model_path = 'models/best_ridge_model.pkl'
+with open(model_path, 'rb') as file:
+    model = pickle.load(file)
 
-st.title("DementiaShield – Predicting Alzheimer’s with Social Insights")
+# User input via file upload
+uploaded_file = st.file_uploader("Upload your data file (CSV)")
 
-# File upload
-uploaded_file = st.file_uploader("Upload a .csv file")
-if uploaded_file:
-    input_data = pd.read_csv(uploaded_file)
+if uploaded_file is not None:
+    user_data = pd.read_csv(uploaded_file)
 
-    # Load and prepare training data (used for initial model training)
-    data = load_data('data/train_features.csv', 'data/train_labels.csv')
-    X = data.drop(['composite_score', 'year'], axis=1)
-    y = data['composite_score']
+    # Preprocess user input
+    preprocessor = preprocess_data(user_data)
+    pipeline = create_pipeline(preprocessor)
 
-    # Train model and evaluate
-    num_cols = X.select_dtypes(include=['float64', 'int64']).columns.tolist()
-    cat_cols = X.select_dtypes(include=['object']).columns.tolist()
-    model, metrics = train_model(X, y, num_cols, cat_cols)
-    
-    st.write("Model Evaluation Metrics:")
-    st.json(metrics)
+    # Transform data and make predictions
+    user_input_transformed = preprocessor.transform(user_data)
+    predictions = model.predict(user_input_transformed)
+
+    st.write("Predictions:", predictions)
